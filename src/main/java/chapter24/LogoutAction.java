24d43f64a292acd7f45e887b0d9928abb4821513
harasan1016 <tks2573403@stu.o-hara.ac.jp>
2026-04-23 10:38:30 +0900

    プロジェクト新規作成

---
+++ src/main/java/chapter24/LogoutAction.java
@@ -0,0 +1,25 @@
+package chapter24;
+
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
+import tool.Action;
+
+public class LogoutAction extends Action {
+	
+	@Override
+	public String execute(HttpServletRequest request, HttpServletResponse response
+	)throws Exception {
+		
+		HttpSession session=request.getSession();
+		
+		
+		if (session.getAttribute("customer")!=null) {
+			session.removeAttribute("customer");
+			return "logout-out.jsp";
+		}
+		
+		return "logout-error.jsp";
+	}
+
+}
---
+++ src/main/webapp/chapter16/attribute2.jsp
@@ -0,0 +1,19 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<%@include file="../header.html" %>
+
+<%@page import ="bean.Student,java.util.List" %>
+
+<%
+p
+ List<Student> list = (List<Student>)request.getAttribute("list")
+%>
+
+<%
+p
+ for (Student p :list)
+%>
+    <%=p.getId() %>:<%=p.getName() %>:<%=p.getPrice() %><br> 
+<% } %> 
+    
+<%@include file="../footer.html" %>	
